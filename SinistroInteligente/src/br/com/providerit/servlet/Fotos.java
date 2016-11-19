e3a3e9bbfa8b8bc4092a9070f26c88fc5c689d26
Allan <Allan@Carlinhos>
2016-11-19 10:27:43 -0200

    Fotos

--- SinistroInteligente/src/br/com/providerit/servlet/Fotos.java
+++ SinistroInteligente/src/br/com/providerit/servlet/Fotos.java
@@ -1,16 +1,21 @@
 package br.com.providerit.servlet;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import br.com.providerit.vo.SinistroVO;
+
 /**
  * Servlet implementation class Fotos
  */
 @WebServlet("/Fotos")
+@MultipartConfig
 public class Fotos extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -26,16 +31,32 @@ public class Fotos extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.getRequestDispatcher("fotos.jsp").forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		String action = request.getParameter("action");
+
+		SinistroVO sinistro = (SinistroVO) request.getSession().getAttribute("sinistro");
+		if ("escolher".equals(action)) {
+//			String description = request.getParameter("description"); // Retrieves <input type="text" name="description">
+//		    Part filePart = request.getPart("file"); // Retrieves <input type="file" name="file">
+//		    String fileName = Paths.get(filePart.getSubmittedFileName()).getFileName().toString(); // MSIE fix.
+//		    InputStream fileContent = filePart.getInputStream();
+	
+			//TODO:Implementar o carinha aqui
+			
+			
+			
+			request.getRequestDispatcher("DescricaoSinistro").forward(request, response);
+		}else{
+			doGet(request, response);
+		}
+		
+		
 	}
 
 }
